99265e6327125f1fd697fd58497a096dd8f04f48
naveedkssk-glitch <naveedkssk@gmail.com>
2025-12-09 13:55:27 +0500

    Reactjs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./app.css"
+
+function App(){
+  return(
+    <div>
+    <h1 className="heading">Muhammad Naveed</h1>
+    <p>I am using reactjs</p>
+    <hr />
+    <br />
+    </div>
+  )
+}
+
+export default App;
