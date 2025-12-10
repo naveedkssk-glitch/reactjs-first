ad7a4f348c37b1fc1cdadea135b49c262146b59c
naveedkssk-glitch <naveedkssk@gmail.com>
2025-12-10 13:28:36 +0500

    reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import "./app.css"
 
-function App(){
-  return(
+function App() {
+  return (
     <div>
-    <h1 className="heading">Muhammad Naveed</h1>
-    <p>I am using reactjs</p>
-    <hr />
-    <br />
+      <h1 className="heading">Muhammad Naveed</h1>
+      <p>I am using reactjs</p>
+      <hr />
+      <br />
+
+      <ol>
+        <li>Ali</li>
+        <li>Asad</li>
+        <li>Fahad</li>
+      </ol>
     </div>
   )
 }
